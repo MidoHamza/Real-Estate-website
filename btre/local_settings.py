af917330315d7d85a77fb82147cfd168fda1cc1a
MidoHamza <42759018+MidoHamza@users.noreply.github.com>
2020-11-04 21:58:09 +0100

    Add files via upload

---
+++ btre/local_settings.py
@@ -0,0 +1,14 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'mz!+z0ji33-d=@qd)c*=lap9kj9ve$$59u#ga-p+tnup=_5o3n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'mz!+z0ji33-d=@qd)c*=lap9kj9ve$$59u#ga-p+tnup=_5o3n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = 
+
